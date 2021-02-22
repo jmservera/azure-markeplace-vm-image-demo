568e63ee4cc378478777ef1e3d78b5f66892386e
Juan Manuel Servera <8036360+jmservera@users.noreply.github.com>
2021-02-22 17:09:50 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,4 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 apt-get install docker-compose 
 
-mkdir /usr/share/nginx
-mkdir /usr/share/nginx/html
-cp ./html /usr/share/nginx/html
-
 docker-compose up -d
