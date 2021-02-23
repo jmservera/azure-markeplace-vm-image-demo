0b443a81b45c89af79f00b667b48483bc011e875
jmservera <8036360+jmservera@users.noreply.github.com>
2021-02-23 14:55:19 +0100

    parametrize script

--- scripts/preparevm-azcli.sh
+++ scripts/preparevm-azcli.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-resourceGroupName="MARKETPLACEVM"
-vmName="marketplacedemo01"
+resourceGroupName="mkttest"
+vmName="mkttest"
 location="northeurope"
+snapshotName="vmsnapshot"
 
-az vm stop -g $resourceGroupName -n $vmName
+sasExpiryDuration=3600
+
 az vm deallocate -g $resourceGroupName -n $vmName
+# to get details: az vm show -g $resourceGroupName -n $vmName --show-details -o table
+
 
 # az disk list to get the diskname
 
-vmDisk=$(az disk list)
-
-az snapshot create -g $resourceGroupName -n vmsnapshot --source $vmDisk
+diskId=$(az vm show -g $resourceGroupName -n $vmName --query storageProfile.osDisk.managedDisk.id -o tsv)
+az snapshot create -g $resourceGroupName -n $snapshotName --source $diskId
+az snapshot wait --created -g $resourceGroupName -n $snapshotName
 
 sas=$(az snapshot grant-access --resource-group $resourceGroupName --name $snapshotName --duration-in-seconds $sasExpiryDuration --query [accessSas] -o tsv)
 
@@ -22,3 +26,5 @@ sas=$(az snapshot grant-access --resource-group $resourceGroupName --name $snaps
 az storage account create -n vmdisksjm01 -g $resourceGroupName --location $location --sku Standard_LRS
 
 az storage container create -n marketplacedisks --account-name vmdisksjm01
+
+# TODO az copy
