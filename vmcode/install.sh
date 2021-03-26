1fabff1eece60c3b2769cc0c52ea9f091599b60e
Juan Manuel Servera <8036360+jmservera@users.noreply.github.com>
2021-03-26 13:16:12 +0100

    Update install.sh

--- vmcode/install.sh
+++ vmcode/install.sh
@@ -2,9 +2,9 @@
 
 echo Installing docker and compose
 curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
+sudo sh get-docker.sh
 
-apt-get install -y docker-compose 
+sudo apt-get install -y docker-compose 
 
 echo Run compose and start the web server
-docker-compose up -d
+sudo docker-compose up -d
